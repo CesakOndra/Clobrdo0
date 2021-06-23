fa114b1a50242b0dfd83e0fd3a91a30bad5b523e
cesak <ondrej.cesak@gjkt.eu>
2021-06-23 14:17:31 +0200

    fxml changes

--- src/com/company/app/Main.java
+++ src/com/company/app/Main.java
@@ -1,9 +1,13 @@
 package com.company.app;
 
+import com.company.view.Controller;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -12,10 +16,21 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../view/rozhrani.fxml"));
         primaryStage.setTitle("Clobrdo");
+
+        vytvoritCestu();
+
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
+    private void vytvoritCestu() {
+        int pocetHracu = 4, delkaTrasy = 10;
+
+        Button[] policka = new Button[pocetHracu * delkaTrasy];
+
+        policka[0] = new Button("Policko");
+        Controller.getGridPane().add(policka[0], 0, 0);
+    }
 
     public static void main(String[] args) {
         launch(args);
--- src/com/company/view/Controller.java
+++ src/com/company/view/Controller.java
@@ -1,6 +1,29 @@
 package com.company.view;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.*;
+
 public class Controller {
 
+    @FXML
+    private static Pane pathPane;
 
+    public static Pane getPathPane() {
+        return pathPane;
+    }
+
+    @FXML
+    private static GridPane gridPane;
+
+    public static GridPane getGridPane() {
+        return gridPane;
+    }
+
+    @FXML
+    private static AnchorPane aPane;
+
+    public static AnchorPane getFlowPane() {
+        return aPane;
+    }
 }
